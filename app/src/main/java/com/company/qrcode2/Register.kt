7ecb7343325c03dcd9bab86af2619da8c8751f77
rianlesmana03 <rianlesmana03@gmail.com>
2022-06-13 11:13:45 +0700

    First Commit

---
+++ app/src/main/java/com/company/qrcode2/Register.kt
@@ -0,0 +1,11 @@
+package com.company.qrcode2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Register : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}
---
+++ app/src/main/java/com/company/qrcode2/SplashScreen.kt
@@ -0,0 +1,11 @@
+package com.company.qrcode2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+    }
+}
